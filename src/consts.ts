222abf7907e0dabc143626618b82eee65fe46d17
Architech User <>
2024-03-16 22:50:16 +0530

    Openresearch site init

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Open Research';
+export const SITE_DESCRIPTION = 'Uncovering the future of knowledge through open access to research notes, simulations, and publications!';
