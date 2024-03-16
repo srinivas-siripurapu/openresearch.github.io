dd6efdb51cb03d98076381ea4d375034148b9f93
Architech User <>
2024-03-16 23:07:28 +0530

    updated site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://srinivas-siripurapu.github.io/openresearch.github.io/',
+	site: 'https://srinivas-siripurapu.github.io',
 	integrations: [mdx(), sitemap()],
 });
